2d7542d5ebe05c25f58a29004951d81e45a7ce6e
trachpro <tu.phamminh.2207@gmail.com>
2018-08-17 06:36:41 +0700

    change to http

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
 const fs = require('fs');
-const https  = require('https');
+// const https  = require('https');
 
 
-console.log("process.env.PORT: ",process.env.PORT);
+// console.log("process.env.PORT: ",process.env.PORT);
 
-const server = https.createServer({
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-}, app).listen(process.env.PORT || port);
+// const server = https.createServer({
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// }, app).listen(process.env.PORT || port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
-
-
-app.setup(server);
-
-logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-
-// const server = app.listen(port);
-// server.on('listening', () =>
-//   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+// process.on('unhandledRejection', (reason, p) =>
+//   logger.error('Unhandled Rejection at: Promise ', p, reason)
 // );
+
+
+// app.setup(server);
+
+// logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+
+const server = app.listen(port);
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
